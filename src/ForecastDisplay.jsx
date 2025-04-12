b77618cd782f2f7e49159e469c82a6244a121cb9
tasneemshaik-collab <tasneemsshaik@email.com>
2025-04-12 09:44:35 +0530

    Final layout with styles and extra buttons

--- src/ForecastDisplay.jsx
+++ src/ForecastDisplay.jsx
@@ -4,14 +4,23 @@ import "./ForecastDisplay.css";
 function ForecastDisplay({ forecast }) {
   return (
     <div className="forecast-container">
-      {forecast.map((item, index) => (
-        <div key={index} className="forecast-card">
-          <h4>{new Date(item.dt_txt).toLocaleDateString(undefined, { weekday: "short" })}</h4>
-          <p>{item.weather[0].main}</p>
-          <p>🌡 {item.main.temp}°C</p>
-          <p>💧 {item.main.humidity}%</p>
-        </div>
-      ))}
+      {forecast.map((item, index) => {
+        const date = new Date(item.dt_txt).toLocaleDateString(undefined, {
+          weekday: "short",
+        });
+        const iconCode = item.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+        return (
+          <div key={index} className="forecast-card">
+            <h4>{date}</h4>
+            <img src={iconUrl} alt={item.weather[0].description} className="forecast-icon" />
+            <p>{item.weather[0].main}</p>
+            <p>🌡 {item.main.temp}°C</p>
+            <p>💧 {item.main.humidity}%</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
